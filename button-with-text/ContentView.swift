25742ba80e976f9ba756035d44da3b2fbd00e7a1
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-20 13:18:19 -0700

    Add Click Me button

--- button-with-text/ContentView.swift
+++ button-with-text/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Button("Click Me!") {
+//            ToDo Action
+        }
             .padding()
     }
 }
