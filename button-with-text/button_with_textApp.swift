16595afde949fb4e59dbeb0e8dbf8410bbd7cd1c
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-20 13:12:22 -0700

    Initial Commit

---
+++ button-with-text/button_with_textApp.swift
@@ -0,0 +1,17 @@
+//
+//  button_with_textApp.swift
+//  button-with-text
+//
+//  Created by user on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct button_with_textApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
